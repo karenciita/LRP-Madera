461a5448cb9a5902022723af10ba8a0dd786657d
TheKePaag <multisport69@gmail.com>
2020-03-15 20:17:48 +0100

    french sql

---
+++ LRP-JOBmedera/localization/fr/items_weight_fr.sql
@@ -0,0 +1,11 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+
+	('hacha', 'Outil de bûcheron', 1, 0, 1),
+	('wood', 'Bois', 1, 0, 1),
+	('cutted_wood', 'Bois coupé', 0.5, 0, 1),
+	
+	('packaged_plank', 'Planche de bois', 0.3, 0, 1),
+	
+	('maderacaoba', "Table en bois d'acajou", 1.5, 0, 1),
+	('maderanogal', 'Table en bois de Noyer', 1.5, 0, 1),
+	('maderaroble', 'Table en bois de Chêne', 1.5, 0, 1);
